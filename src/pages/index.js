a25620b921e15435a34854fa5893339d6b91cc3b
Kean Sum Ooi <kean@forestcoins.io>
2018-08-31 22:01:10 +0800

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi 2018 freeCodeCamp.org JAMStack Hackathon!</h1>
+    <p>Keano ready to see what interesting things await!</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
